03967bacde4beebd369641bc5136f01c5da9b172
tleza0258 <68152988+tleza0258@users.noreply.github.com>
2020-08-03 22:19:33 +0700

    Add files via upload

---
+++ calculate 10 year.py
@@ -0,0 +1,37 @@
+amount = float (input("Enter amount :"))
+rate = float (input("Enter rate :"))
+year = float (input("Enter year :"))
+
+FV = amount * (1 + rate /100) ** year
+print("Future Value = ",FV)
+
+print ("=========================")
+amount = amount * (1 + rate /100)
+print("Future Value = ",amount)
+
+amount = amount * (1 + rate /100)
+print("Future Value = ",amount)
+
+amount = amount * (1 + rate /100)
+print("Future Value = ",amount)
+
+amount = amount * (1 + rate /100)
+print("Future Value = ",amount)
+
+amount = amount * (1 + rate /100)
+print("Future Value = ",amount)
+
+amount = amount * (1 + rate /100)
+print("Future Value = ",amount)
+
+amount = amount * (1 + rate /100)
+print("Future Value = ",amount)
+
+amount = amount * (1 + rate /100)
+print("Future Value = ",amount)
+
+amount = amount * (1 + rate /100)
+print("Future Value = ",amount)
+
+amount = amount * (1 + rate /100)
+print("Future Value = ",amount)
